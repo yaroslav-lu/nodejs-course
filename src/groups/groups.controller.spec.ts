c310818a54340dcce9dd22c32e2eaafd9fe0a12c
Yaroslav <yariklutsiuk@gmail.com>
2023-03-07 18:54:59 +0200

    Task 7: Controllers testing.

--- src/groups/groups.controller.spec.ts
+++ src/groups/groups.controller.spec.ts
@@ -1,20 +1,155 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { GroupsController } from './groups.controller';
 import { GroupsService } from './groups.service';
+import { CreateGroupDto } from './dto/create-group.dto';
+import { UpdateGroupDto } from './dto/update-group.dto';
+import { faker } from '@faker-js/faker';
+import { Permission } from './enums/perminssion.enum';
+import { AddUserDto } from './dto/add-user.dto';
+
+const firstGroupUuid = faker.datatype.uuid();
+const firstGroup: CreateGroupDto = {
+  name: faker.internet.userName(),
+  permissions: [Permission.READ],
+};
+
+const secondGroupUuid = faker.datatype.uuid();
+const secondGroup: CreateGroupDto = {
+  name: faker.internet.userName(),
+  permissions: [Permission.UPLOAD_FILES],
+};
+
+const updateGroupDto: UpdateGroupDto = {
+  permissions: [Permission.WRITE],
+};
+
+const addUserDto: AddUserDto = {
+  users: [faker.datatype.uuid()],
+};
 
 describe('GroupsController', () => {
-  let controller: GroupsController;
+  let groupsController: GroupsController;
+  let groupsService: GroupsService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [GroupsController],
-      providers: [GroupsService],
+      providers: [
+        GroupsService,
+        {
+          provide: GroupsService,
+          useValue: {
+            create: jest.fn().mockImplementation((group: CreateGroupDto) =>
+              Promise.resolve({
+                id: firstGroupUuid,
+                ...group,
+              }),
+            ),
+            findAll: jest.fn().mockResolvedValue([firstGroup, secondGroup]),
+            findOne: jest.fn().mockImplementation((id: string) =>
+              Promise.resolve({
+                id,
+                ...firstGroup,
+              }),
+            ),
+            update: jest
+              .fn()
+              .mockImplementation(
+                (id: string, updateGroupDto: UpdateGroupDto) =>
+                  Promise.resolve({
+                    id,
+                    ...updateGroupDto,
+                  }),
+              ),
+            remove: jest.fn().mockReturnValue(true),
+            addUser: jest
+              .fn()
+              .mockImplementation((id: string, addUserDto: AddUserDto) =>
+                Promise.resolve({
+                  id,
+                  ...firstGroup,
+                  ...addUserDto,
+                }),
+              ),
+          },
+        },
+      ],
     }).compile();
 
-    controller = module.get<GroupsController>(GroupsController);
+    groupsController = module.get<GroupsController>(GroupsController);
+    groupsService = module.get<GroupsService>(GroupsService);
   });
 
   it('should be defined', () => {
-    expect(controller).toBeDefined();
+    expect(groupsController).toBeDefined();
+  });
+
+  describe('create()', () => {
+    it('should create a group', () => {
+      groupsController.create(firstGroup);
+      expect(groupsController.create(firstGroup)).resolves.toEqual({
+        id: firstGroupUuid,
+        ...firstGroup,
+      });
+      expect(groupsService.create).toHaveBeenCalledWith(firstGroup);
+    });
+  });
+
+  describe('findAll()', () => {
+    it('should find all groups ', () => {
+      groupsController.findAll();
+      expect(groupsService.findAll).toHaveBeenCalled();
+    });
+  });
+
+  describe('findOne()', () => {
+    it('should find a group', () => {
+      const res = groupsController.findOne(firstGroupUuid);
+      expect(groupsController.findOne(firstGroupUuid)).resolves.toEqual({
+        id: firstGroupUuid,
+        ...firstGroup,
+      });
+      expect(groupsService.findOne).toHaveBeenCalledWith(firstGroupUuid);
+    });
+  });
+
+  describe('update()', () => {
+    it('should update a group', () => {
+      groupsController.update(firstGroupUuid, updateGroupDto);
+      expect(
+        groupsController.update(firstGroupUuid, updateGroupDto),
+      ).resolves.toEqual({
+        id: firstGroupUuid,
+        ...updateGroupDto,
+      });
+      expect(groupsService.update).toHaveBeenCalledWith(
+        firstGroupUuid,
+        updateGroupDto,
+      );
+    });
+  });
+
+  describe('remove()', () => {
+    it('should remove the group', () => {
+      groupsController.remove(secondGroupUuid);
+      expect(groupsService.remove).toHaveBeenCalledWith(secondGroupUuid);
+    });
+  });
+
+  describe('addUser()', () => {
+    it('should add user to the group', () => {
+      groupsController.addUser(firstGroupUuid, addUserDto);
+      expect(
+        groupsController.addUser(firstGroupUuid, addUserDto),
+      ).resolves.toEqual({
+        id: firstGroupUuid,
+        ...firstGroup,
+        ...addUserDto,
+      });
+      expect(groupsService.addUser).toHaveBeenCalledWith(
+        firstGroupUuid,
+        addUserDto,
+      );
+    });
   });
 });
--- src/users/users.controller.spec.ts
+++ src/users/users.controller.spec.ts
@@ -1,20 +1,149 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { UsersController } from './users.controller';
 import { UsersService } from './users.service';
+import { CreateUserDto } from './dto/create-user.dto';
+import { faker } from '@faker-js/faker';
+import { PaginationUserQueryDto } from './dto/pagination-user-query.dto';
+import { UpdateUserDto } from './dto/update-user.dto';
+
+const firstUserUuid = faker.datatype.uuid();
+const firstUser: CreateUserDto = {
+  login: faker.internet.userName(),
+  password: faker.internet.password(20, true, /[a-zA-Z]/),
+  age: faker.datatype.number({ min: 4, max: 130 }),
+  groups: [],
+};
+
+const secondUserUuid = faker.datatype.uuid();
+const secondUser: CreateUserDto = {
+  login: faker.internet.userName(),
+  password: faker.internet.password(20, true, /[a-zA-Z]/),
+  age: faker.datatype.number({ min: 4, max: 130 }),
+  groups: [],
+};
+
+const updateUserDto: UpdateUserDto = {
+  login: faker.internet.userName(),
+  age: faker.datatype.number({ min: 4, max: 130 }),
+};
+
+const paginationUserQueryDto: PaginationUserQueryDto = {
+  loginSubstring: '',
+  limit: 100,
+};
 
 describe('UsersController', () => {
-  let controller: UsersController;
+  let usersController: UsersController;
+  let usersService: UsersService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [UsersController],
-      providers: [UsersService],
+      providers: [
+        UsersService,
+        {
+          provide: UsersService,
+          useValue: {
+            create: jest.fn().mockImplementation((user: CreateUserDto) =>
+              Promise.resolve({
+                id: firstUserUuid,
+                isDeleted: false,
+                ...user,
+              }),
+            ),
+            findAll: jest.fn().mockResolvedValue([firstUser, secondUser]),
+            findOne: jest.fn().mockImplementation((id: string) =>
+              Promise.resolve({
+                id,
+                isDeleted: false,
+                ...firstUser,
+              }),
+            ),
+            update: jest
+              .fn()
+              .mockImplementation((id: string, updateUserDto: UpdateUserDto) =>
+                Promise.resolve({
+                  id,
+                  isDeleted: false,
+                  ...updateUserDto,
+                }),
+              ),
+            remove: jest.fn().mockImplementation((id: string) =>
+              Promise.resolve({
+                id,
+                isDeleted: true,
+                ...secondUser,
+              }),
+            ),
+          },
+        },
+      ],
     }).compile();
 
-    controller = module.get<UsersController>(UsersController);
+    usersController = module.get<UsersController>(UsersController);
+    usersService = module.get<UsersService>(UsersService);
   });
 
   it('should be defined', () => {
-    expect(controller).toBeDefined();
+    expect(usersController).toBeDefined();
+  });
+
+  describe('create()', () => {
+    it('should create a user', () => {
+      usersController.create(firstUser);
+      expect(usersController.create(firstUser)).resolves.toEqual({
+        id: firstUserUuid,
+        isDeleted: false,
+        ...firstUser,
+      });
+      expect(usersService.create).toHaveBeenCalledWith(firstUser);
+    });
+  });
+
+  describe('findAll()', () => {
+    it('should find all users ', () => {
+      usersController.findAll(paginationUserQueryDto);
+      expect(usersService.findAll).toHaveBeenCalledWith(paginationUserQueryDto);
+    });
+  });
+
+  describe('findOne()', () => {
+    it('should find a user', () => {
+      expect(usersController.findOne(firstUserUuid)).resolves.toEqual({
+        id: firstUserUuid,
+        isDeleted: false,
+        ...firstUser,
+      });
+      expect(usersService.findOne).toHaveBeenCalledWith(firstUserUuid);
+    });
+  });
+
+  describe('update()', () => {
+    it('should update a user', () => {
+      usersController.update(firstUserUuid, updateUserDto);
+      expect(
+        usersController.update(firstUserUuid, updateUserDto),
+      ).resolves.toEqual({
+        id: firstUserUuid,
+        isDeleted: false,
+        ...updateUserDto,
+      });
+      expect(usersService.update).toHaveBeenCalledWith(
+        firstUserUuid,
+        updateUserDto,
+      );
+    });
+  });
+
+  describe('remove()', () => {
+    it('should soft remove the user', () => {
+      usersController.remove(secondUserUuid);
+      expect(usersController.remove(secondUserUuid)).resolves.toEqual({
+        id: secondUserUuid,
+        isDeleted: true,
+        ...secondUser,
+      });
+      expect(usersService.remove).toHaveBeenCalledWith(secondUserUuid);
+    });
   });
 });
